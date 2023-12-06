cf20572a0f201876c547f4b9288c2c3708089015
maris <gulayrosemaris@gmail.com>
2023-12-06 10:41:55 +0800

    updated selenium

--- SeleniumFirst/SeleniumFirst/Program.cs
+++ SeleniumFirst/SeleniumFirst/Program.cs
@@ -1,15 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
 
 namespace SeleniumFirst
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            // Create an instance of ChromeDriver
+            IWebDriver driver = new ChromeDriver();
+
+            // Navigate to the Google page
+            driver.Navigate().GoToUrl("http://www.google.com");
+
+            // Find the search input element by its name attribute
+            IWebElement element = driver.FindElement(By.Name("q"));
+
+            // Type "executeautomation" into the search box
+            element.SendKeys("facebook");
+
+            // Submit the form (press Enter)
+            element.Submit();
+
+            // Wait for a while to see the results (you might want to use WebDriverWait for more robust waiting)
+            System.Threading.Thread.Sleep(5000);
+
+            // Close the browser
+            driver.Close();
         }
     }
 }
