640d4d0a9fdadf0ff6cda51965f6ad4d2183d5b5
maris <gulayrosemaris@gmail.com>
2023-12-06 12:12:07 +0800

    added fab link

--- SeleniumFirst/SeleniumFirst/Program.cs
+++ SeleniumFirst/SeleniumFirst/Program.cs
@@ -12,19 +12,34 @@ namespace SeleniumFirst
             IWebDriver driver = new ChromeDriver();
 
             // Navigate to the Google page
-            driver.Navigate().GoToUrl("http://www.google.com");
+            driver.Navigate().GoToUrl("https://www.facebook.com/");
 
             // Find the search input element by its name attribute
-            IWebElement element = driver.FindElement(By.Name("q"));
+            IWebElement emailElement = driver.FindElement(By.Name("email"));
+            IWebElement passwordElement = driver.FindElement(By.Name("pass"));
 
             // Type "executeautomation" into the search box
-            element.SendKeys("facebook");
+            emailElement.SendKeys("gulayrosemaris@gmail.com");
+            passwordElement.SendKeys("iloveyoumaris");
+
+            //
 
             // Submit the form (press Enter)
-            element.Submit();
+            passwordElement.Submit();
 
             // Wait for a while to see the results (you might want to use WebDriverWait for more robust waiting)
-            System.Threading.Thread.Sleep(5000);
+            System.Threading.Thread.Sleep(10000);
+            driver.Navigate().GoToUrl("https://www.facebook.com/richard.tinampay.50");
+            System.Threading.Thread.Sleep(30000);
+
+            // Play the video (replace 'play_button_selector' with the actual selector)
+           // var playButtonSelector = "your_play_button_selector";
+           // var playButton = driver.FindElement(By.CssSelector(playButtonSelector));
+            //playButton.Click();
+
+            // Wait for the video to play (adjust the sleep duration as needed)
+          //  Thread.Sleep(10000);
+
 
             // Close the browser
             driver.Close();
